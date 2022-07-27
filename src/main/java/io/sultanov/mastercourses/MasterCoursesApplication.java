98a5ca3b0d6233c82dff70994bdf9bb45c9c1da6
yergoff <ruslanja1@mail.ru>
2022-07-27 14:06:59 +0500

    initial commit

---
+++ src/main/java/io/sultanov/mastercourses/MasterCoursesApplication.java
@@ -0,0 +1,13 @@
+package io.sultanov.mastercourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterCoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sultanov/mastercourses/MasterCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sultanov.mastercourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
